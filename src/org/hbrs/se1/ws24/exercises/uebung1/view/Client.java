171342248c53cd9d9984ec0518bb2fe82e98a35e
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-10-09 15:04:52 +0200

    u01

--- src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
@@ -1,26 +1,18 @@
 package org.hbrs.se1.ws24.exercises.uebung1.view;
 
+import org.hbrs.se1.ws24.exercises.uebung1.control.*;
+
 public class Client {
 
-		/**
-		 * Methode zur Ausgabe einer Zahl auf der Console
-		 * (auch bezeichnet als CLI, Terminal)
-		 *
-		 */
-		 void display( int aNumber ){
-			// In dieser Methode soll die Methode translateNumber
-			// mit dem übergegebenen Wert der Variable aNumber
-			// aufgerufen werden.
-			//
-			// Strenge Implementierung (nur) gegen das Interface Translator gewuenscht!
+  /**
+   * Methode zur Ausgabe einer Zahl auf der Console
+   * (auch bezeichnet als CLI, Terminal)
+   *
+   */
+  void display (int aNumber) {
+    Translator translator = TranslatorFactory.createTranslator(TranslatorFactory.TranslatorType.GERMAN);
+    String ergebnis = translator.translateNumber(aNumber);
 
-			 System.out.println("Das Ergebnis der Berechnung: " +
-					"[das Ergebnis an dieser Stelle]" );
-
-		 }
+    System.out.println("Das Ergebnis der Berechnung: " + ergebnis);
+  }
 }
-
-
-
-
-
