2eedee6469683d6d81eec22a0895add4795b7488
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-10-09 15:04:52 +0200

    u01

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
@@ -2,30 +2,30 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 
 public class GermanTranslator implements Translator {
 
-	public String date = "Okt/2024"; // Default-Wert
+  public String date = "Okt/2024"; // Default-Wert
 
-	/**
-	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
-	 */
-	public String translateNumber( int number ) {
-		// [ihr Source Code aus Übung 1-2]
+  /**
+   * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
+   */
+  public String translateNumber (int number) {
+    String[] nummern = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"};
+    
+    try {
+      return nummern[number - 1];
+    } catch (Exception e) {
+      return "Übersetzung der Zahl " + number + " nicht möglich (Version " + this.version + ")";
+    }
+  }
 
-		return "null";
-	}
+  /**
+   * Objektmethode der Klasse GermanTranslator zur Ausgabe einer Info.
+   */
+  public void printInfo () { System.out.println( "GermanTranslator v1.9, erzeugt am " + this.date ); }
 
-	/**
-	 * Objektmethode der Klasse GermanTranslator zur Ausgabe einer Info.
-	 */
-	public void printInfo(){
-		System.out.println( "GermanTranslator v1.9, erzeugt am " + this.date );
-	}
-
-	/**
-	 * Setzen des Datums, wann der Uebersetzer erzeugt wurde (Format: Monat/Jahr (Beispiel: "Okt/2024"))
-	 * Das Datum sollte system-intern durch eine Factory-Klasse gesetzt werden und nicht von externen View-Klassen
-	 */
-	public void setDate( String date ) {
-		this.date = date;
-	}
+  /**
+   * Setzen des Datums, wann der Uebersetzer erzeugt wurde (Format: Monat/Jahr (Beispiel: "Okt/2024"))
+   * Das Datum sollte system-intern durch eine Factory-Klasse gesetzt werden und nicht von externen View-Klassen
+   */
+  public void setDate (String date) { this.date = date; }
 
 }
