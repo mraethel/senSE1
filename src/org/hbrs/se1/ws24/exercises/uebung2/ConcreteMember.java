35e54e43a4d323809cdab7675b4ff57ac8a5cd7d
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-10-12 22:25:51 +0200

    ~ ConcreteMember (incremental ID)

--- src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
@@ -2,14 +2,16 @@ package org.hbrs.se1.ws24.exercises.uebung2;
 
 public class ConcreteMember implements Member {
 
+  private static Integer nextId = Integer.MIN_VALUE;
+
   private Integer id;
 
-  public ConcreteMember () { id = Long.valueOf(System.nanoTime()).hashCode(); }
+  public ConcreteMember () { id = nextId++; }
 
   @Override
-  public Integer getID() { return id; }
+  public Integer getID () { return id; }
 
   @Override
-  public String toString() { return getID().toString(); }
+  public String toString () { return getID().toString(); }
 
 }
