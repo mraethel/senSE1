e7044a80bd4fe76a8b0df7da8267dcf23b5f56a9
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-10-16 20:41:22 +0200

    + uebung3.plantuml + ContainerTestU3.class

---
+++ test/org/hbrs/se1/ws24/tests/uebung3/ContainerTestU3.java
@@ -0,0 +1,17 @@
+package org.hbrs.se1.ws24.tests.uebung3;
+
+import org.hbrs.se1.ws24.exercises.uebung3.*;
+
+import org.hbrs.se1.ws24.tests.uebung2.ContainerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ContainerTestU3 extends ContainerTest {
+  @Override
+  public void init() {
+    
+  }
+}
