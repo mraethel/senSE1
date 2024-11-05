a573b10e25421bb465b7ffff6ba06c6a8ab968c5
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-11-06 00:09:27 +0100

    dunno

--- src/org/hbrs/se1/ws24/exercises/uebung4/Main.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/Main.java
@@ -7,27 +7,103 @@ import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceException;
 import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceStrategyStream;
 import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
 
-import picocli.CommandLine;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+import java.io.PrintWriter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
+import picocli.shell.jline3.PicocliCommands.PicocliCommandsFactory;
+import picocli.shell.jline3.PicocliCommands;
 
-import java.util.List;
-import java.util.Collections;
+import org.jline.reader.LineReader;
+import org.jline.reader.Parser;
+import org.jline.reader.Binding;
+import org.jline.reader.impl.DefaultParser;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.reader.MaskingCallback;
+import org.jline.terminal.Terminal;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.console.SystemRegistry;
+import org.jline.console.impl.SystemRegistryImpl;
+
+import org.fusesource.jansi.AnsiConsole;
 
 @Command(name = "main",
-    subcommands = { Enter.class,
-                    Store.class,
-                    Load.class,
-                    Dump.class,
-                    CommandLine.HelpCommand.class },
+    subcommands = { Enter.class
+                  , Store.class
+                  , Load.class
+                  , Dump.class
+                  , CommandLine.HelpCommand.class
+                  },
     description = "Verwaltung von User-Stories.")
 public class Main {
+
+  PrintWriter out;
 
   public static void main(String[] args) {
     Container.INSTANCE.setPersistenceStrategy(new PersistenceStrategyStream<>());
 
-    int exitCode = new CommandLine(new Main()).execute(args);
-    System.exit(exitCode);
+    AnsiConsole.systemInstall();
+    try {
+      Supplier<Path> workDir = () -> Paths.get(System.getProperty("user.dir"));
+
+      Main commands = new Main();
+
+      PicocliCommandsFactory factory = new PicocliCommandsFactory();
+
+      CommandLine cmd = new CommandLine(commands, factory);
+      PicocliCommands picocliCommands = new PicocliCommands(cmd);
+      
+      Parser parser = new DefaultParser();
+      try (Terminal terminal = TerminalBuilder.builder().build()) {
+        SystemRegistry systemRegistry = new SystemRegistryImpl(parser, terminal, workDir, null);
+        systemRegistry.setCommandRegistries(picocliCommands);
+        systemRegistry.register("help", picocliCommands);
+
+        LineReader reader = LineReaderBuilder.builder()
+          .terminal(terminal)
+          .completer(systemRegistry.completer())
+          .parser(parser)
+          .variable(LineReader.LIST_MAX, 50)
+          .build();
+        commands.setReader(reader);
+        factory.setTerminal(terminal);
+
+        String prompt = "> ";
+        String rightPrompt = null;
+
+        String line;
+        while (true) {
+          try {
+            systemRegistry.cleanUp();
+            line = reader.readLine(prompt, rightPrompt, (MaskingCallback) null, null);
+            systemRegistry.execute(line);
+          } catch (UserInterruptException e) {
+          } catch (EndOfFileException e) {
+            return;
+          } catch (Exception e) {
+            systemRegistry.trace(e);
+          }
+        }
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+    } finally {
+      AnsiConsole.systemUninstall();
+    }
+  }
+
+  public void setReader(LineReader reader) {
+    out = reader.getTerminal().writer();
   }
 
 }
@@ -71,14 +147,14 @@ class Enter implements Runnable {
   @Override
   public void run() {
     try {
-      Container.INSTANCE.addMember(new UserStory(
-          titel,
-          akzeptanz,
-          mehrwert,
-          strafe,
-          risiko,
-          aufwand,
-          projekt));
+      Container.INSTANCE.addMember(new UserStory(titel
+                                                ,akzeptanz
+                                                ,mehrwert
+                                                ,strafe
+                                                ,risiko
+                                                ,aufwand
+                                                ,projekt
+                                                ));
     } catch (ContainerException e) {
       System.out.println("Es existiert bereits eine User-Story mit der ID " + e.getMessage() + "!");
     };
@@ -120,7 +196,8 @@ class Dump implements Runnable {
   @Override
   public void run() {
     List<Member> sortedContainer = Container.INSTANCE.getCurrentList();
-    Collections.sort(sortedContainer);
+//  Collections.sort(sortedContainer);
     sortedContainer.forEach(System.out::println);
   }
+
 }
