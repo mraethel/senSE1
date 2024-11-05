75601d2d8da34a9c6b7219ea2bf5a1d1838883cd
Maximilian Raethel <maximilian.raethel@smail.inf.h-brs.de>
2024-11-05 16:42:20 +0100

    ~ Main.class (picocli)

--- src/org/hbrs/se1/ws24/exercises/uebung4/Main.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/Main.java
@@ -1,11 +1,113 @@
 package org.hbrs.se1.ws24.exercises.uebung4;
 
-import org.apache.commons.cli.CommandLine;
+import org.hbrs.se1.ws24.exercises.uebung2.Container;
+import org.hbrs.se1.ws24.exercises.uebung2.ContainerException;
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
 
-import java.util.Arrays;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
+@Command(name = "Main",
+    subcommands = { Enter.class,
+                    Store.class,
+                    Load.class,
+                    Dump.class,
+                    CommandLine.HelpCommand.class },
+    description = "Verwaltung von User-Stories.")
 public class Main {
+
   public static void main(String[] args) {
-    Arrays.asList(args).forEach(System.out::println);
+    int exitCode = new CommandLine(new Main()).execute(args);
+    System.exit(exitCode);
+  }
+
+}
+
+@Command(name = "enter",
+    description = "Eingabe einer User-Story.")
+class Enter implements Runnable {
+  
+  @Parameters(index = "0",
+      description = "Titel der User-Story.")
+  private String titel;
+
+  @Parameters(index = "1",
+      description = "akzeptanz")
+  private String akzeptanz;
+
+  @Parameters(index = "2",
+      description = "mehrwert")
+  private Mehrwert mehrwert;
+
+  @Parameters(index = "3",
+      description = "strafe")
+  private Strafe strafe;
+
+  @Parameters(index = "4",
+      description = "risiko")
+  private Risiko risiko;
+
+  @Parameters(index = "5",
+      description = "Geschätzter Aufwand der User-Story.")
+  private Aufwand aufwand;
+
+  @Parameters(index = "6",
+      description = "Zugehöriges Projekt.")
+  private String projekt;
+
+  @Override
+  public void run() {
+    try {
+      Container.INSTANCE.addMember(new UserStory(
+          titel,
+          akzeptanz,
+          mehrwert,
+          strafe,
+          risiko,
+          aufwand,
+          projekt));
+    } catch (ContainerException e) {
+      System.out.println("Es existiert bereits eine User-Story mit der ID " + e.getMessage() + "!");
+    };
+  }
+}
+
+@Command(name = "store",
+    description = "Speichern von geladenen User-Stories.")
+class Store implements Runnable {
+
+  @Override
+  public void run() {
+    try {
+      Container.INSTANCE.store();
+    } catch (PersistenceException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
+
+@Command(name = "load",
+    description = "Laden von User-Stories.")
+class Load implements Runnable {
+
+  @Override
+  public void run() {
+    try {
+      Container.INSTANCE.load();
+    } catch (PersistenceException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}
+
+@Command(name = "dump",
+    description = "Ausgebe geladener User-Stories.")
+class Dump implements Runnable {
+
+  @Override
+  public void run() {
+    
   }
 }
--- src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
@@ -27,76 +27,76 @@ public class UserStory extends ConcreteMember {
     this.projekt = projekt;
   }
 
-  public class Mehrwert {
-    
-    private char val;
+}
 
-    public Mehrwert(char val) throws IllegalArgumentException {
-      if (0 < val && val < 6)
-        throw new IllegalArgumentException("Mehrwert");
+class Mehrwert {
 
-      this.val = val;
-    }
+  private char val;
 
-    public char val() { return val; }
+  public Mehrwert(char val) throws IllegalArgumentException {
+    if (0 < val && val < 6)
+      throw new IllegalArgumentException("Mehrwert");
 
+    this.val = val;
   }
 
-  public class Strafe {
-    
-    private char val;
+  public char val() { return val; }
 
-    public Strafe(char val) throws IllegalArgumentException {
-      if (0 < val && val < 6)
-        throw new IllegalArgumentException("Strafe");
+}
 
-      this.val = val;
-    }
+class Strafe {
 
-    public char val() { return val; }
+  private char val;
 
+  public Strafe(char val) throws IllegalArgumentException {
+    if (0 < val && val < 6)
+      throw new IllegalArgumentException("Strafe");
+
+    this.val = val;
   }
 
-  public class Risiko {
-    
-    private char val;
+  public char val() { return val; }
 
-    public Risiko(char val) throws IllegalArgumentException {
-      if (0 < val && val < 6)
-        throw new IllegalArgumentException("Risiko");
+}
 
-      this.val = val;
-    }
-    
-    public char val() { return val; }
+class Risiko {
 
+  private char val;
+
+  public Risiko(char val) throws IllegalArgumentException {
+    if (0 < val && val < 6)
+      throw new IllegalArgumentException("Risiko");
+
+    this.val = val;
   }
 
-  public class Aufwand {
-    
-    private char val;
+  public char val() { return val; }
 
-    public Aufwand(char val) throws IllegalArgumentException {
-      if (0 < val && val < 6)
-        throw new IllegalArgumentException("Aufwand");
+}
 
-      this.val = val;
-    }
+class Aufwand {
 
-    public char val() { return val; }
+  private char val;
 
+  public Aufwand(char val) throws IllegalArgumentException {
+    if (0 < val && val < 6)
+      throw new IllegalArgumentException("Aufwand");
+
+    this.val = val;
   }
 
-  public class Prio {
-    
-    private float val;
+  public char val() { return val; }
 
-    public Prio(char mehrwert, char strafe, char risiko, char aufwand) {
-      val = (mehrwert + strafe) / (aufwand + risiko);
-    }
+}
 
-    public float val() { return val; }
+class Prio {
 
+  private float val;
+
+  public Prio(char mehrwert, char strafe, char risiko, char aufwand) {
+    val = (mehrwert + strafe) / (aufwand + risiko);
   }
+
+  public float val() { return val; }
 
 }
